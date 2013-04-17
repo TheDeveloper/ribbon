69dd44fd29991323c8198388717cc5f746c4f7ef
Geoff Wagstaff <geoffwagstaff@gmail.com>
2013-04-17 01:18:42 +0100

    A couple of extra test cases and logic

--- test/adaptor-amqp.js
+++ test/adaptor-amqp.js
@@ -29,6 +29,24 @@ describe('AMQP adaptor', function(){
     });
   });
 
+  it('invokes callback without error when starting up while already up', function(done){
+    c.startUp(done);
+  });
+
+  it('invokes all callbacks successfully when multiple calls to startup are made', function(done){
+    var i = 15, pending = i;
+    var cb = function(err){
+      if(!--pending || err){
+        return done(err);
+      }
+    };
+
+    var amqp = ribbon.wrap('amqp', amqpOpts);
+    while(i--){
+      amqp.startUp(cb);
+    }
+  });
+
   it('invokes callback with error on failed startup', function(done){
     setUpConnection({}, function(err, adaptor){
       should.exist(err);
